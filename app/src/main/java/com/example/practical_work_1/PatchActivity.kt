7acd829e409f2ea61ab1eecddc7c65cb3d6f22ae
user <mob5@wsr.ru>
2023-01-11 10:03:23 +0500

    new

--- app/src/main/java/com/example/practical_work_1/PatchActivity.kt
+++ app/src/main/java/com/example/practical_work_1/PatchActivity.kt
@@ -3,18 +3,32 @@ package com.example.practical_work_1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 
 class PatchActivity : AppCompatActivity() {
+
+    lateinit var tb:Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patch)
+
+        tb = findViewById(R.id.toolbar)
+        setSupportActionBar(this.tb)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title="Кино"
+
     }
 
-    fun back(view: View) {
-        val intent = Intent(this,SigninActivity::class.java)
-        startActivity(intent)
-        finish()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        return true
     }
 
 }
