28dcd047b9358852f10d4cfcfe75c05573189e20
user <mob5@wsr.ru>
2023-01-20 14:22:09 +0500

    new 4.3

---
+++ app/src/main/java/com/example/practical_work_1/Fragment3.kt
@@ -0,0 +1,28 @@
+package com.example.practical_work_1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Fragment3 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_3, container, false)
+    }
+
+/*    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Fragment3().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }*/
+}
