acaa6cd5738037549d9df7ae69e2a2ae2c577b23
root <admin@example.com>
2023-02-10 12:47:54 +0500

    new 6.1...

--- app/src/main/java/com/example/practical_work_1/FragmentScreen2.kt
+++ app/src/main/java/com/example/practical_work_1/FragmentScreen2.kt
@@ -1,14 +1,13 @@
 package com.example.practical_work_1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 
 class FragmentScreen2 : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
